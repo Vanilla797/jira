23cf2d7974b462b06ffca3df06990a90b5d2d7bb
adam <616177709@qq.com>
2022-01-24 23:41:45 +0800

    feat:customer hooks

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -3,7 +3,7 @@ import List from "./list";
 import { useState, useEffect } from "react";
 import React from "react";
 import * as qs from "qs";
-import cleanObject from "../../utils";
+import cleanObject, { useDebounce, useMount } from "../../utils";
 const apiUrl = process.env.REACT_APP_API_URL;
 
 const ProjectListScreen = () => {
@@ -12,25 +12,27 @@ const ProjectListScreen = () => {
     personId: "",
   });
 
+  const deBouncedParam = useDebounce(param, 500);
+
   const [list, setList] = useState([]);
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
-      async (response) => {
-        if (response.ok) {
-          setList(await response.json());
-        }
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(deBouncedParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
       }
-    );
-  }, [param]);
-  useEffect(() => {
+    });
+  }, [deBouncedParam]);
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json());
       }
     });
-  }, [param]);
+  });
 
   return (
     <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export default function cleanObject(object) {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
@@ -8,3 +10,18 @@ export default function cleanObject(object) {
   });
   return result;
 }
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debouncedValue;
+};
